307172d0fb4ce7301e42d3be63488aa8db4ebdbe
George Wang <georgetw1108@gmail.com>
2015-05-30 11:39:14 -0700

    Added kill target on trigger that kills fake cat.

--- Game/Assets/Scripts/KillTargetOnTrigger.cs
+++ Game/Assets/Scripts/KillTargetOnTrigger.cs
@@ -5,9 +5,14 @@ public class KillTargetOnTrigger : MonoBehaviour {
 
 	public EnemyDamageReceiver DamageReceiver;
 	public GameObject RequiredCollision;
+	public float Damage;
 
 	public void OnTriggerEnter(Collider other)
 	{
-		Debug.Log("Trigger enter");
+		Debug.Log("Trigger enter " + other.gameObject);
+		if (RequiredCollision == null || other.gameObject == RequiredCollision)
+		{
+			DamageReceiver.ApplyDamage(Damage);
+		}
 	}
 }
