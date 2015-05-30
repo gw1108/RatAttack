29e8c9458b4f882c17bd49a75a8f3a1bd4a7ae30
George Wang <georgetw1108@gmail.com>
2015-05-30 11:35:38 -0700

    Added some small stuff. Deleted char controller. Added push things to player obj. Added ice material to everything kinda so things slip and slide.

---
+++ Game/Assets/Scripts/KillTargetOnTrigger.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KillTargetOnTrigger : MonoBehaviour {
+
+	public EnemyDamageReceiver DamageReceiver;
+	public GameObject RequiredCollision;
+
+	public void OnTriggerEnter(Collider other)
+	{
+		Debug.Log("Trigger enter");
+	}
+}
