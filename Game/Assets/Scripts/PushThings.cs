020461525b552abbe6129c1d6feacc00f26b704a
George Wang <georgetw1108@gmail.com>
2015-05-30 12:57:06 -0700

    Better sound. Fail sounds.

--- Game/Assets/Scripts/PushThings.cs
+++ Game/Assets/Scripts/PushThings.cs
@@ -1,44 +1,63 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Timers;
+using System.Diagnostics;
 
 public class PushThings : MonoBehaviour
 {
 	public float pushPower = 5.0f;
 	public AudioClip SFX;
+	public AudioClip FailSFX;
+	public float timeBetweenSFX = 2.0f;
 	public float volume = 0.5f;
+
+	private Stopwatch timer;
 	private AudioSource m_source;
 
 	void Start()
 	{
 		m_source = GetComponent<AudioSource>();
+		timer = new Stopwatch();
+		timer.Start();
 	}
 
 	void OnControllerColliderHit(ControllerColliderHit hit)
 	{
-		if (hit.collider.attachedRigidbody == null || hit.collider.attachedRigidbody.isKinematic)
+		if (hit.collider.attachedRigidbody == null)
 			return;
 
 		// We dont want to push objects below us
 		if (hit.moveDirection.y < -0.3f)
 			return;
 
-		// Calculate push direction from move direction,
-		// we only push objects to the sides never up and down
-		var pushDir = new Vector3(hit.moveDirection.x, 0, hit.moveDirection.z);
-
-		// If you know how fast your character is trying to move,
-		// then you can also multiply the push velocity by that.
-
-		// Apply the push
-		hit.collider.attachedRigidbody.velocity = pushDir * pushPower;
-		if (!m_source.isPlaying)
+		if (hit.collider.attachedRigidbody.isKinematic)
 		{
-			m_source.clip = SFX;
-			float oldVolume = m_source.volume;
+			UnityEngine.Debug.Log("PLAY FAIL SOUND");
+			PlayAudioClipNonRepeat(FailSFX);
+		}
+		else
+		{
+			// Calculate push direction from move direction,
+			// we only push objects to the sides never up and down
+			var pushDir = new Vector3(hit.moveDirection.x, 0, hit.moveDirection.z);
+
+			// If you know how fast your character is trying to move,
+			// then you can also multiply the push velocity by that.
+
+			// Apply the push
+			hit.collider.attachedRigidbody.velocity = pushDir * pushPower;
+			PlayAudioClipNonRepeat(SFX);
+		}
+	}
+
+	private void PlayAudioClipNonRepeat(AudioClip sfx)
+	{
+		if (timer.ElapsedMilliseconds / 1000.0f > timeBetweenSFX)
+		{
+			timer.Reset();
+			timer.Start();
 			m_source.volume = volume;
-			m_source.Play();
-			m_source.volume = oldVolume;
-			//m_source.PlayOneShot(SFX);
+			m_source.PlayOneShot(sfx);
 		}
 	}
 }
