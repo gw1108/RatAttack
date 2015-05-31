ce182511612ae4138b605834b4f4d33c5e4a646e
George Wang <georgetw1108@gmail.com>
2015-05-31 00:25:42 -0700

    Added cat attack trigger. Cat is now angry.

---
+++ Game/Assets/Scripts/CatAttackTrigger.cs
@@ -0,0 +1,47 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// - Confidential Information                                                                                          -
+// - Copyright © 2012-2015, Obsidian Entertainment, Inc.                                                               -
+// - All rights reserved.                                                                                              -
+// - Created by gwang at 5/30/2015 11:55:48 PM																			-
+// ---------------------------------------------------------------------------------------------------------------------
+
+using UnityEngine;
+using System.Diagnostics;
+
+public class CatAttackTrigger : MonoBehaviour
+{
+	public Animation catAnim;
+	public string attack = "CatSitAttack";
+	public float attackSpeed = 1.0f;
+	public float attackDuration = 1.0f;
+	public string idle = "SitIdle";
+
+	private Stopwatch timer = new Stopwatch();
+
+	public void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.layer == LayerMask.NameToLayer("Player"))
+		{
+			CatAttack();
+			timer.Reset();
+			timer.Start();
+		}
+	}
+
+	private void CatAttack()
+	{
+		catAnim[attack].speed = attackSpeed;
+		catAnim.PlayQueued(attack, QueueMode.PlayNow);
+		catAnim.PlayQueued(idle);
+	}
+
+	public void OnTriggerStay(Collider other)
+	{
+		if (timer.ElapsedMilliseconds / 1000.0f > attackDuration && other.gameObject.layer == LayerMask.NameToLayer("Player") && !PlayerHealth.dead)
+		{
+			timer.Reset();
+			timer.Start();
+			CatAttack();
+		}
+	}
+}
